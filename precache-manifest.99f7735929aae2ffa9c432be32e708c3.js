fcd649859e02b2d4feef8295ef5f697b2410fc9b
danddycandy <danddycandy@gmail.com>
2023-07-25 01:33:46 +0900

    Updates

---
+++ precache-manifest.99f7735929aae2ffa9c432be32e708c3.js
@@ -0,0 +1,502 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc8df9d21031182a8968f339b6f3a33",
+    "url": "/Portfolio_Wannacfuture/index.html"
+  },
+  {
+    "revision": "afcefe2e01ceafcf470e9ec0bc5244b0",
+    "url": "/Portfolio_Wannacfuture/index.html.gz"
+  },
+  {
+    "revision": "38af014dad07c139a81f",
+    "url": "/Portfolio_Wannacfuture/static/css/1.7d15fde6.chunk.css"
+  },
+  {
+    "revision": "17b6b5cd289e35b1736ec614da0077f5",
+    "url": "/Portfolio_Wannacfuture/static/css/1.7d15fde6.chunk.css.gz"
+  },
+  {
+    "revision": "de95c75e091c14501c1244034c1922f1",
+    "url": "/Portfolio_Wannacfuture/static/css/1.7d15fde6.chunk.css.map.gz"
+  },
+  {
+    "revision": "a68fbc76e4909015b5f7",
+    "url": "/Portfolio_Wannacfuture/static/css/2.e7d1dd7a.chunk.css"
+  },
+  {
+    "revision": "ee5e96a19193632441101cf3e45b7e4c",
+    "url": "/Portfolio_Wannacfuture/static/css/2.e7d1dd7a.chunk.css.gz"
+  },
+  {
+    "revision": "c76f7adf61547c40fce200cc1770ec30",
+    "url": "/Portfolio_Wannacfuture/static/css/2.e7d1dd7a.chunk.css.map.gz"
+  },
+  {
+    "revision": "0842ebe920624fe73346",
+    "url": "/Portfolio_Wannacfuture/static/css/3.b3c10813.chunk.css"
+  },
+  {
+    "revision": "049c4849329ebae5e9c2f9416c8e18f6",
+    "url": "/Portfolio_Wannacfuture/static/css/3.b3c10813.chunk.css.gz"
+  },
+  {
+    "revision": "5a26f4f8de0c331608119acae1c9bc09",
+    "url": "/Portfolio_Wannacfuture/static/css/3.b3c10813.chunk.css.map.gz"
+  },
+  {
+    "revision": "953f08d8e70b89d7a4a0",
+    "url": "/Portfolio_Wannacfuture/static/css/main.4ca3257f.chunk.css"
+  },
+  {
+    "revision": "c2528b948acd9064d1703e385942cbc2",
+    "url": "/Portfolio_Wannacfuture/static/css/main.4ca3257f.chunk.css.gz"
+  },
+  {
+    "revision": "650fea927cde560e1407485c7f8386c6",
+    "url": "/Portfolio_Wannacfuture/static/css/main.4ca3257f.chunk.css.map.gz"
+  },
+  {
+    "revision": "76eef5e65f76569dceaf",
+    "url": "/Portfolio_Wannacfuture/static/js/0.e61fe6b9.chunk.js"
+  },
+  {
+    "revision": "98cbfcdfa85b1fa7dd1ee5de69a5d2bd",
+    "url": "/Portfolio_Wannacfuture/static/js/0.e61fe6b9.chunk.js.gz"
+  },
+  {
+    "revision": "c33d00fbe1ebe597048c245309c5dbcc",
+    "url": "/Portfolio_Wannacfuture/static/js/0.e61fe6b9.chunk.js.map.gz"
+  },
+  {
+    "revision": "38af014dad07c139a81f",
+    "url": "/Portfolio_Wannacfuture/static/js/1.997e7490.chunk.js"
+  },
+  {
+    "revision": "dd27fb59263d97939d74d98e64361742",
+    "url": "/Portfolio_Wannacfuture/static/js/1.997e7490.chunk.js.gz"
+  },
+  {
+    "revision": "17934707834744f699e28d7187a4fb3c",
+    "url": "/Portfolio_Wannacfuture/static/js/1.997e7490.chunk.js.map.gz"
+  },
+  {
+    "revision": "a68fbc76e4909015b5f7",
+    "url": "/Portfolio_Wannacfuture/static/js/2.ecb41d30.chunk.js"
+  },
+  {
+    "revision": "fe75cbd38a8658349c4e86bb331df44b",
+    "url": "/Portfolio_Wannacfuture/static/js/2.ecb41d30.chunk.js.gz"
+  },
+  {
+    "revision": "8f359cbd3c3942d1b947a221825f31e5",
+    "url": "/Portfolio_Wannacfuture/static/js/2.ecb41d30.chunk.js.map.gz"
+  },
+  {
+    "revision": "0842ebe920624fe73346",
+    "url": "/Portfolio_Wannacfuture/static/js/3.6f0b95a5.chunk.js"
+  },
+  {
+    "revision": "7bde8e3b3d6f86a90a5c0a2d9d96c438",
+    "url": "/Portfolio_Wannacfuture/static/js/3.6f0b95a5.chunk.js.gz"
+  },
+  {
+    "revision": "5415ac0dfe20730b287aaf44e0baf809",
+    "url": "/Portfolio_Wannacfuture/static/js/3.6f0b95a5.chunk.js.map.gz"
+  },
+  {
+    "revision": "8e90e0b1c48bbeaf03d9",
+    "url": "/Portfolio_Wannacfuture/static/js/6.55e4b355.chunk.js"
+  },
+  {
+    "revision": "28354543ca6d8f4cdb8bfbadaaa8b698",
+    "url": "/Portfolio_Wannacfuture/static/js/6.55e4b355.chunk.js.gz"
+  },
+  {
+    "revision": "3dc0d54bbac91db33aa161de51844830",
+    "url": "/Portfolio_Wannacfuture/static/js/6.55e4b355.chunk.js.map.gz"
+  },
+  {
+    "revision": "953f08d8e70b89d7a4a0",
+    "url": "/Portfolio_Wannacfuture/static/js/main.b6851fe4.chunk.js"
+  },
+  {
+    "revision": "67958b83223724abe5aa88f398ab2474",
+    "url": "/Portfolio_Wannacfuture/static/js/main.b6851fe4.chunk.js.gz"
+  },
+  {
+    "revision": "f1aaf42dc42e6d07d61ecd8e00abad4c",
+    "url": "/Portfolio_Wannacfuture/static/js/main.b6851fe4.chunk.js.map.gz"
+  },
+  {
+    "revision": "df01863503e0ca4b1c3f",
+    "url": "/Portfolio_Wannacfuture/static/js/runtime~main.4af4fadd.js"
+  },
+  {
+    "revision": "4e9b4cf584f5563ac869f92c2879cbd1",
+    "url": "/Portfolio_Wannacfuture/static/js/runtime~main.4af4fadd.js.gz"
+  },
+  {
+    "revision": "7c4a1ecb46e203fcc07a5f0ebce22505",
+    "url": "/Portfolio_Wannacfuture/static/js/runtime~main.4af4fadd.js.map.gz"
+  },
+  {
+    "revision": "717d81a5c47658775be7e08b70257c34",
+    "url": "/Portfolio_Wannacfuture/static/media/1.717d81a5.png"
+  },
+  {
+    "revision": "75800ce78ccd3aa270f5884ae78e8df1",
+    "url": "/Portfolio_Wannacfuture/static/media/1.75800ce7.png"
+  },
+  {
+    "revision": "7708930e02a502b11321ef2f22e6dfa3",
+    "url": "/Portfolio_Wannacfuture/static/media/1.7708930e.png"
+  },
+  {
+    "revision": "8139b9758843123bfd5f4a7747d4a850",
+    "url": "/Portfolio_Wannacfuture/static/media/1.8139b975.png"
+  },
+  {
+    "revision": "c53761d46bbe7763ef95300efa43e1e0",
+    "url": "/Portfolio_Wannacfuture/static/media/1.c53761d4.png"
+  },
+  {
+    "revision": "d6e23e0159d024138306a3cd32ca264f",
+    "url": "/Portfolio_Wannacfuture/static/media/1.d6e23e01.png"
+  },
+  {
+    "revision": "dc237dfb23a7e4d0146a6609f2f110ab",
+    "url": "/Portfolio_Wannacfuture/static/media/1.dc237dfb.png"
+  },
+  {
+    "revision": "fe333dc8aefa4f4d53e0576c82e655e2",
+    "url": "/Portfolio_Wannacfuture/static/media/1.fe333dc8.png"
+  },
+  {
+    "revision": "05f48c8c7d6b5d939c4d37eb95c862c8",
+    "url": "/Portfolio_Wannacfuture/static/media/2.05f48c8c.png"
+  },
+  {
+    "revision": "07480575ac5fb54d8cffa65af7bf3852",
+    "url": "/Portfolio_Wannacfuture/static/media/2.07480575.png"
+  },
+  {
+    "revision": "2684a0de58bfa186eac94c24e60c587e",
+    "url": "/Portfolio_Wannacfuture/static/media/2.2684a0de.png"
+  },
+  {
+    "revision": "3eadd6cf29501dae0ac52c1d27d89eab",
+    "url": "/Portfolio_Wannacfuture/static/media/2.3eadd6cf.png"
+  },
+  {
+    "revision": "b06df1f298c5b283855189667c35e1c5",
+    "url": "/Portfolio_Wannacfuture/static/media/2.b06df1f2.png"
+  },
+  {
+    "revision": "cd008a15573df876f93c294e56872e22",
+    "url": "/Portfolio_Wannacfuture/static/media/2.cd008a15.png"
+  },
+  {
+    "revision": "f39123d9f4c71098b65e35e014865eaf",
+    "url": "/Portfolio_Wannacfuture/static/media/2.f39123d9.png"
+  },
+  {
+    "revision": "0b0320a8ded6d834b95c6e53145dbc61",
+    "url": "/Portfolio_Wannacfuture/static/media/3.0b0320a8.png"
+  },
+  {
+    "revision": "20e8b5d6eb29c5db92bd4213d5c985e6",
+    "url": "/Portfolio_Wannacfuture/static/media/3.20e8b5d6.png"
+  },
+  {
+    "revision": "2cf43baed8dca715ae23ee106a7a9b39",
+    "url": "/Portfolio_Wannacfuture/static/media/3.2cf43bae.png"
+  },
+  {
+    "revision": "5d2686c0dbd98cd1a25b686c6524ebe7",
+    "url": "/Portfolio_Wannacfuture/static/media/3.5d2686c0.png"
+  },
+  {
+    "revision": "66405f71fdbd4265acd3a3f372c57cf8",
+    "url": "/Portfolio_Wannacfuture/static/media/3.66405f71.png"
+  },
+  {
+    "revision": "850ba6f952bf3d740cb09055857ce6a4",
+    "url": "/Portfolio_Wannacfuture/static/media/3.850ba6f9.png"
+  },
+  {
+    "revision": "9ba76c0eab9b37561a1ec2417ad3bdd9",
+    "url": "/Portfolio_Wannacfuture/static/media/3.9ba76c0e.png"
+  },
+  {
+    "revision": "f7e9d0776001a8fc29db1f8a0f266d3e",
+    "url": "/Portfolio_Wannacfuture/static/media/3.f7e9d077.png"
+  },
+  {
+    "revision": "10571499234de4c52c3495bd9648c94d",
+    "url": "/Portfolio_Wannacfuture/static/media/4.10571499.png"
+  },
+  {
+    "revision": "2826ed5356e9ee196c51968ae160b2df",
+    "url": "/Portfolio_Wannacfuture/static/media/4.2826ed53.png"
+  },
+  {
+    "revision": "66cb866775e2c3f2c0ead89f9b29c19b",
+    "url": "/Portfolio_Wannacfuture/static/media/4.66cb8667.png"
+  },
+  {
+    "revision": "84b75f8cd5cec5df9afd3833d5456031",
+    "url": "/Portfolio_Wannacfuture/static/media/4.84b75f8c.png"
+  },
+  {
+    "revision": "88fcc13571c14716ee2f519def7611d7",
+    "url": "/Portfolio_Wannacfuture/static/media/4.88fcc135.png"
+  },
+  {
+    "revision": "9c2e71c7656477147a598e172a11858b",
+    "url": "/Portfolio_Wannacfuture/static/media/4.9c2e71c7.png"
+  },
+  {
+    "revision": "c8dff5d4e51c89e60dacefda972f1a52",
+    "url": "/Portfolio_Wannacfuture/static/media/4.c8dff5d4.png"
+  },
+  {
+    "revision": "fe031c91988e327c8b616bb0f601affa",
+    "url": "/Portfolio_Wannacfuture/static/media/4.fe031c91.png"
+  },
+  {
+    "revision": "1ff6f190a44115970c9944effad2b848",
+    "url": "/Portfolio_Wannacfuture/static/media/5.1ff6f190.png"
+  },
+  {
+    "revision": "313c72065ed895ea7d50f93cbd6eca1f",
+    "url": "/Portfolio_Wannacfuture/static/media/5.313c7206.png"
+  },
+  {
+    "revision": "59e5d2feec6a9ee4f3308738d8bc6f3e",
+    "url": "/Portfolio_Wannacfuture/static/media/5.59e5d2fe.png"
+  },
+  {
+    "revision": "605c95a81e1bd6d85011096b617a3ec7",
+    "url": "/Portfolio_Wannacfuture/static/media/5.605c95a8.png"
+  },
+  {
+    "revision": "b82f29c797e7aa762d0b8477d7b874a6",
+    "url": "/Portfolio_Wannacfuture/static/media/5.b82f29c7.png"
+  },
+  {
+    "revision": "da4ccb0b1e84779a6886364f993c6c09",
+    "url": "/Portfolio_Wannacfuture/static/media/5.da4ccb0b.png"
+  },
+  {
+    "revision": "e8d2090f7d96f0ec9e3734412b7302fe",
+    "url": "/Portfolio_Wannacfuture/static/media/5.e8d2090f.png"
+  },
+  {
+    "revision": "ea6ad16c1c14bf84542bd911d2d8af5f",
+    "url": "/Portfolio_Wannacfuture/static/media/5.ea6ad16c.png"
+  },
+  {
+    "revision": "054ee3cf927928654765eb282af57491",
+    "url": "/Portfolio_Wannacfuture/static/media/6.054ee3cf.png"
+  },
+  {
+    "revision": "1125f8362ee8dc7446eef485a6a43360",
+    "url": "/Portfolio_Wannacfuture/static/media/6.1125f836.png"
+  },
+  {
+    "revision": "6685057e09fd1cc48027013a73ec2e2d",
+    "url": "/Portfolio_Wannacfuture/static/media/6.6685057e.png"
+  },
+  {
+    "revision": "bb1328222fa728ed8bf831be9f0860f8",
+    "url": "/Portfolio_Wannacfuture/static/media/6.bb132822.png"
+  },
+  {
+    "revision": "d62e2118451dba05ce75670ed3d06416",
+    "url": "/Portfolio_Wannacfuture/static/media/6.d62e2118.png"
+  },
+  {
+    "revision": "d8392ae2ccb57d5a31e3f9c484731424",
+    "url": "/Portfolio_Wannacfuture/static/media/6.d8392ae2.png"
+  },
+  {
+    "revision": "feafdb045b1bc4d60c091ce4fe7ff2e2",
+    "url": "/Portfolio_Wannacfuture/static/media/6.feafdb04.png"
+  },
+  {
+    "revision": "0e35bc468ca1d5e3df817cedae386d69",
+    "url": "/Portfolio_Wannacfuture/static/media/7.0e35bc46.png"
+  },
+  {
+    "revision": "498be053090d31cb48ad677f36075933",
+    "url": "/Portfolio_Wannacfuture/static/media/7.498be053.png"
+  },
+  {
+    "revision": "5d99d3a4ad0e5d7225ab6450c5d5b87f",
+    "url": "/Portfolio_Wannacfuture/static/media/7.5d99d3a4.png"
+  },
+  {
+    "revision": "5e47ed0da1dc548d4db0df60b48b6aa3",
+    "url": "/Portfolio_Wannacfuture/static/media/7.5e47ed0d.png"
+  },
+  {
+    "revision": "6dd799feffedb9c443e96b28e4a9c420",
+    "url": "/Portfolio_Wannacfuture/static/media/7.6dd799fe.png"
+  },
+  {
+    "revision": "892d54a9b8cce0f3354abacc8f9e2862",
+    "url": "/Portfolio_Wannacfuture/static/media/7.892d54a9.png"
+  },
+  {
+    "revision": "029fcf5a94365a7271b7fc227098f2ee",
+    "url": "/Portfolio_Wannacfuture/static/media/8.029fcf5a.png"
+  },
+  {
+    "revision": "0fd3454f74b6ab5c4bf1c24ef9f334df",
+    "url": "/Portfolio_Wannacfuture/static/media/8.0fd3454f.png"
+  },
+  {
+    "revision": "da4653c7aababa814bc9ac4dbb04fc49",
+    "url": "/Portfolio_Wannacfuture/static/media/8.da4653c7.png"
+  },
+  {
+    "revision": "ffa35aa165914ebd9436c0db9df57f8e",
+    "url": "/Portfolio_Wannacfuture/static/media/8.ffa35aa1.png"
+  },
+  {
+    "revision": "c14d800926d6a33d526e13b9eb82759c",
+    "url": "/Portfolio_Wannacfuture/static/media/9-1.c14d8009.png"
+  },
+  {
+    "revision": "787464428aca520eea7ed25179b581ad",
+    "url": "/Portfolio_Wannacfuture/static/media/9-2.78746442.png"
+  },
+  {
+    "revision": "2166f64911bbac79f4a8b4e43d528a70",
+    "url": "/Portfolio_Wannacfuture/static/media/9.2166f649.png"
+  },
+  {
+    "revision": "658debd5d10ebd94d8f9fd1abe82d267",
+    "url": "/Portfolio_Wannacfuture/static/media/background-dark-doodle-first-layer.658debd5.png"
+  },
+  {
+    "revision": "eabc78e2793a31cadc4f245af98f5098",
+    "url": "/Portfolio_Wannacfuture/static/media/background-dark-doodle-fixed-layer.eabc78e2.png"
+  },
+  {
+    "revision": "49f0fd2730aecbe44b475f3e6f109d21",
+    "url": "/Portfolio_Wannacfuture/static/media/background-dark-doodle-second-layer.49f0fd27.png"
+  },
+  {
+    "revision": "0c19e9a7254356baf3c41c7504cb026a",
+    "url": "/Portfolio_Wannacfuture/static/media/background-image-SUTD.0c19e9a7.jpg"
+  },
+  {
+    "revision": "2937c743cb5c498eb2c3796032a4b966",
+    "url": "/Portfolio_Wannacfuture/static/media/background-image-college.2937c743.jpg"
+  },
+  {
+    "revision": "ee66bb19b4788750a2fcb30c9319c2c7",
+    "url": "/Portfolio_Wannacfuture/static/media/background-image-nykaa.ee66bb19.jpg"
+  },
+  {
+    "revision": "2b1c92607993ba9641b978e46fb86de9",
+    "url": "/Portfolio_Wannacfuture/static/media/background-image-projects.2b1c9260.jpg"
+  },
+  {
+    "revision": "ad4142b2f07eafd2fb2c1dd9798ae411",
+    "url": "/Portfolio_Wannacfuture/static/media/background-image-tailoredtech.ad4142b2.jpg"
+  },
+  {
+    "revision": "43b277c9773731259f913630cc2806b7",
+    "url": "/Portfolio_Wannacfuture/static/media/career-logo-first.43b277c9.png"
+  },
+  {
+    "revision": "38741094e53a152a1f1c7cd725a4aeac",
+    "url": "/Portfolio_Wannacfuture/static/media/career-logo-rest.38741094.png"
+  },
+  {
+    "revision": "935613ccb7b674dd6ce0e07f433c370a",
+    "url": "/Portfolio_Wannacfuture/static/media/electron-background-image.935613cc.png"
+  },
+  {
+    "revision": "9bb9fa9179d3afa1e31bc756b76f517b",
+    "url": "/Portfolio_Wannacfuture/static/media/laravel-background-image.9bb9fa91.png"
+  },
+  {
+    "revision": "eb50f93f9ca8fa6015e50b1a43d1d8bb",
+    "url": "/Portfolio_Wannacfuture/static/media/mit-logo-first.eb50f93f.png"
+  },
+  {
+    "revision": "9b9426cee26985ed708bfcc3dbb52e1e",
+    "url": "/Portfolio_Wannacfuture/static/media/mit-logo-rest.9b9426ce.png"
+  },
+  {
+    "revision": "4653d35478ceec989dbc212e4251b3be",
+    "url": "/Portfolio_Wannacfuture/static/media/nykaa-logo-rest.4653d354.png"
+  },
+  {
+    "revision": "b5f1d6775d2fa2a3a03b648c88be4926",
+    "url": "/Portfolio_Wannacfuture/static/media/pngwing.com (1).b5f1d677.png"
+  },
+  {
+    "revision": "a82065ff7a0267fad02643d01b247efb",
+    "url": "/Portfolio_Wannacfuture/static/media/profile-pic.a82065ff.jpeg"
+  },
+  {
+    "revision": "bb7e92490f504177362cddc591872620",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-andersen.bb7e9249.png"
+  },
+  {
+    "revision": "b5f1d6775d2fa2a3a03b648c88be4926",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-bachelor.b5f1d677.png"
+  },
+  {
+    "revision": "fe2e8cf1a3ad59cd4977dc7371bc9dcb",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-cap.fe2e8cf1.png"
+  },
+  {
+    "revision": "ffae404ecd4f42308cbeb6a48c50c192",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-lighthouse.ffae404e.png"
+  },
+  {
+    "revision": "7ddc2c37e2ebb68364b8684935cb1e02",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-measure.7ddc2c37.png"
+  },
+  {
+    "revision": "3222747a175be3cbf15a6e3eee3a2e80",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-nykaa.3222747a.png"
+  },
+  {
+    "revision": "f878ec64dfd29cebe7578f2d23658719",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-pulse.f878ec64.png"
+  },
+  {
+    "revision": "d0f412505045d5e277bdc60ea34b3e58",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-rakuten.d0f41250.png"
+  },
+  {
+    "revision": "65c49428e97b259be4fa6b9d618226a6",
+    "url": "/Portfolio_Wannacfuture/static/media/project-icon-vc-music-player.65c49428.png"
+  },
+  {
+    "revision": "4721162b69a2a6b33744bd28887332a2",
+    "url": "/Portfolio_Wannacfuture/static/media/projects-logo-first.4721162b.png"
+  },
+  {
+    "revision": "2cafbeefc35b4f7a471b14cc19bf050e",
+    "url": "/Portfolio_Wannacfuture/static/media/projects-logo-rest.2cafbeef.png"
+  },
+  {
+    "revision": "2199849b765d419675807371bc198ea3",
+    "url": "/Portfolio_Wannacfuture/static/media/react-background-image.2199849b.png"
+  },
+  {
+    "revision": "5c657d3bda0fb9a2e7b4db71fd07594e",
+    "url": "/Portfolio_Wannacfuture/static/media/react-native-background-image.5c657d3b.png"
+  },
+  {
+    "revision": "3aed0545dec9d10e80cc9b5bfdac902a",
+    "url": "/Portfolio_Wannacfuture/static/media/tech-doodle-background-image.3aed0545.png"
+  }
+]);
